15658084f1e344995508eff489f17d95c94caf51
Weslley Costa <weslleydasilvacostaa@gmail.com>
2022-07-11 20:24:45 -0300

    other components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,22 @@
+import Header from "./components/Header"
 import NotificationButton from "./components/NotificationButton"
+import SalesCard from "./components/SalesCard"
 
 
 function App() {
-  
+
 
   return (
-    < NotificationButton/>
+    <>
+      <Header />
+      <main>
+        <section id="sales">
+          <div className="dsmeta-container">
+            <SalesCard/>
+          </div>
+        </section>
+      </main>
+    </>
   )
 }
 
