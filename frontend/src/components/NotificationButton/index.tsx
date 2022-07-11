871014a9a68b2baf38d394f3b3ce94ef3cd60a54
Weslley Costa <weslleydasilvacostaa@gmail.com>
2022-07-11 19:57:08 -0300

    First component

---
+++ frontend/src/components/NotificationButton/index.tsx
@@ -0,0 +1,11 @@
+import Icon from '../../assets/img/notification-icon.svg'
+import './styles.css'
+function NotificationButton(){
+    return ( 
+        <div className="dsmeta-red-btn">
+            <img src={Icon} alt="Notificar" />
+        </div>
+    )
+}
+
+export default NotificationButton
