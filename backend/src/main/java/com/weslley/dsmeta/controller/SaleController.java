d36ae8ed995ff0e90d08bcd989f7775d62b4fd65
Weslley Costa <weslleydasilvacostaa@gmail.com>
2022-07-14 22:12:35 -0300

    Date Select

--- backend/src/main/java/com/weslley/dsmeta/controller/SaleController.java
+++ backend/src/main/java/com/weslley/dsmeta/controller/SaleController.java
@@ -3,9 +3,12 @@ package com.weslley.dsmeta.controller;
 import com.weslley.dsmeta.entities.Sale;
 import com.weslley.dsmeta.service.SaleService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -17,7 +20,10 @@ public class SaleController {
     private SaleService service;
 
     @GetMapping
-    public List<Sale> findSales(){
-        return service.findSales();
+    public Page<Sale> findSales(
+            @RequestParam(value = "minDate", defaultValue = "") String minDate,
+            @RequestParam(value = "maxDate", defaultValue = "")String maxDate,
+            Pageable pageable){
+        return service.findSales(minDate, maxDate, pageable);
     }
 }
