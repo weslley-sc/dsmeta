8226df39d2267696daffcb93a51c28bce16f3e6b
Weslley Costa <weslleydasilvacostaa@gmail.com>
2022-07-16 15:43:00 -0300

    twilio

--- backend/src/main/java/com/weslley/dsmeta/controller/SaleController.java
+++ backend/src/main/java/com/weslley/dsmeta/controller/SaleController.java
@@ -2,14 +2,12 @@ package com.weslley.dsmeta.controller;
 
 import com.weslley.dsmeta.entities.Sale;
 import com.weslley.dsmeta.service.SaleService;
+import com.weslley.dsmeta.service.SmsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -19,11 +17,19 @@ public class SaleController {
     @Autowired
     private SaleService service;
 
+    @Autowired
+    private SmsService smsService;
+
     @GetMapping
     public Page<Sale> findSales(
             @RequestParam(value = "minDate", defaultValue = "") String minDate,
             @RequestParam(value = "maxDate", defaultValue = "")String maxDate,
             Pageable pageable){
         return service.findSales(minDate, maxDate, pageable);
+    }
+
+    @GetMapping("/{id}/notification")
+    public void notifySms(@PathVariable Long id){
+        smsService.sendSms(id);
     }
 }
